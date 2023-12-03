f805a9110b91738d3057b459813eadd4f51ca8ea
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-03 18:08:09 +0000

    day 2 part 1 working solution

---
+++ Day 2/day2.js
@@ -0,0 +1,56 @@
+const fs = require("node:fs");
+
+// modify arrays to take min and max operators
+// taken from: https://stackoverflow.com/questions/1669190/find-the-min-max-element-of-an-array-in-javascript
+Array.prototype.max = function () {
+  return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function () {
+  return Math.min.apply(null, this);
+};
+
+function findColourMax(colour, game) {
+  let regex = new RegExp(`\\d+(?=\\s${colour})`, "g");
+  return game.match(regex).map(Number).max();
+}
+
+// read input file
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // split input file at line breaks
+  let games = data.split(/\r?\n/);
+  let games_IDs = [];
+  let red_nums = [];
+  let green_nums = [];
+  let blue_nums = [];
+  let red_total = 12;
+  let green_total = 13;
+  let blue_total = 14;
+  let ID_total = 0;
+
+  games.forEach((game) => {
+    // push the ID number of each game to an array called games_IDs
+    // push the count of each colour to an array for that colour per game
+    games_IDs.push(Number(game.match(/(?<=Game\s)\d+(?=:)/g)));
+    red_nums.push(findColourMax("red", game));
+    green_nums.push(findColourMax("green", game));
+    blue_nums.push(findColourMax("blue", game));
+  });
+  // check if the highest number of each colour shown is less than the given values
+  // if they are, add the ID to a running total
+  for (i = 0; i < games_IDs.length; i++) {
+    if (
+      red_nums[i] <= red_total &&
+      green_nums[i] <= green_total &&
+      blue_nums[i] <= blue_total
+    ) {
+      ID_total += games_IDs[i];
+    }
+  }
+  console.log(ID_total);
+});
