8b50cebbeaa64457109e547c79c5e9b13b4d6720
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-06 17:47:07 +0000

    day 6 part 2 working solution

--- Day 6/day6.js
+++ Day 6/day6.js
@@ -8,30 +8,20 @@ fs.readFile("input.txt", "utf8", (err, data) => {
   data = data.split(/\r?\n/);
   let details = {};
   for (i in data) {
-    let match = data[i]
-      .match(/(?<=:\s+)\d.*/g)[0]
-      .split(/\s/)
-      .filter((element) => element.trim() !== "")
-      .map(Number);
+    let match = data[i].match(/(?<=:\s+)\d.*/g)[0].replace(/\s/g, "");
+    match = parseInt(match, 10);
     let key = data[i].match(/\w*(?=:)/g)[0].toLowerCase();
     details[key] = match;
   }
   let acceleration = 1;
-  let number_wins_arr = [];
 
-  for (i = 0; i < details.time.length; i++) {
-    let number_wins = 0;
-    for (hold_time = 1; hold_time < details.time[i]; hold_time++) {
-      velocity = hold_time * acceleration; // v = u + at where u = 0
-      let total_distance = velocity * (details.time[i] - hold_time); // d = s * t
-      if (total_distance > details.distance[i]) {
-        number_wins += 1;
-      }
+  let number_wins = 0;
+  for (hold_time = 1; hold_time < details.time; hold_time++) {
+    velocity = hold_time * acceleration; // v = u + at where u = 0
+    let total_distance = velocity * (details.time - hold_time); // d = s * t
+    if (total_distance > details.distance) {
+      number_wins += 1;
     }
-    number_wins_arr.push(number_wins);
   }
-
-  console.log(`Number of wins for each race respectively: ${number_wins_arr}`);
-  margin_of_error = number_wins_arr.reduce((a, b) => a * b, 1);
-  console.log(`Margin of error is ${margin_of_error}`);
+  console.log(`Number of wins: ${number_wins}`);
 });
