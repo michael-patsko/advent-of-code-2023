bc1d9ef08e6f21f3235e760c04d0bbe50a876441
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-08 13:30:35 +0000

    day 7 part 2 working solution

--- Day 7/day7.js
+++ Day 7/day7.js
@@ -36,7 +36,10 @@ function assignTypes(handCounts) {
   // 0 = High Card, 1 = One Pair, 2 = Two Pair, 3 = Three of a Kind, 4 = Full House, 5 = Four of a Kind, 6 = Full House
   const handTypes = new Map();
   handCounts.forEach((count, hand) => {
-    let type = types.findIndex((element) => element === countCharacters(hand));
+    console.log(`hand: ${hand}`);
+    charCount = countCharacters(hand);
+    let type = types.findIndex((element) => element === charCount);
+    console.log(`Hand type: ${type}`);
     handTypes.set(hand, type);
   });
   return handTypes;
@@ -45,6 +48,7 @@ function assignTypes(handCounts) {
 function compareHands(handA, handB, handTypes) {
   // this will be used to rank hands where they have the same type
   const cardRanks = [
+    "J",
     "2",
     "3",
     "4",
@@ -54,7 +58,6 @@ function compareHands(handA, handB, handTypes) {
     "8",
     "9",
     "T",
-    "J",
     "Q",
     "K",
     "A",
@@ -81,14 +84,139 @@ function compareHands(handA, handB, handTypes) {
 
 function countCharacters(str) {
   // counts the characters in a string and returns it as a string of counts
+
   let charCounts = str.split("").reduce((acc, char) => {
     acc[char] = (acc[char] || 0) + 1;
     return acc;
   }, {});
-  return Object.values(charCounts)
+  charCounts = replaceJokers(charCounts);
+  let charString = Object.values(charCounts)
     .sort((a, b) => b - a)
     .join("")
-    .toString();
+    .toString(); // get string of counts for matching type
+  console.log(
+    `Jokers replaced. New charString: ${Object.keys(charCounts)
+      .join("")
+      .toString()}`
+  );
+  return charString;
+}
+
+function replaceJokers(charCounts) {
+  // this function takes an object with the counts of cards in a hand, and distributes the jokers to create the most valuable hand
+  const cardRanks = [
+    "J",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "T",
+    "Q",
+    "K",
+    "A",
+  ];
+  const jokerCount = charCounts["J"];
+  delete charCounts["J"];
+  charsWithoutJ = Object.keys(charCounts).filter((key) => key !== "J");
+  switch (jokerCount) {
+    case 0:
+      return charCounts; // no jokers to replace
+    case 1:
+      if (charsWithoutJ.length === 1) {
+        let char = charsWithoutJ[0];
+        return (charCounts = {
+          [char]: 5,
+        });
+      } else if (charsWithoutJ.length === 4) {
+        // give to highest value card using cardRanks.findIndex to get highest value card
+        let char = charsWithoutJ.reduce((a, b) =>
+          cardRanks.findIndex((element) => element === a) >
+          cardRanks.findIndex((element) => element === b)
+            ? a
+            : b
+        );
+        charCounts[char] += jokerCount;
+        return charCounts;
+      } else if (charsWithoutJ.length === 3) {
+        // give to highest count card
+        let char = keysWithHighestValue(charCounts)[0];
+        charCounts[char] += jokerCount;
+        return charCounts;
+      } else {
+        let chars = keysWithHighestValue(charCounts);
+        // add to character with highest count
+        if (chars.length === 1) {
+          let char = chars[0];
+          charCounts[char] += jokerCount;
+          return charCounts;
+        } else {
+          // find character with highest value and add joker to that
+          let char = charsWithoutJ.reduce((a, b) =>
+            cardRanks.findIndex((element) => element === a) >
+            cardRanks.findIndex((element) => element === b)
+              ? a
+              : b
+          );
+          charCounts[char] += jokerCount;
+          return charCounts;
+        }
+      }
+    case 2:
+      if (charsWithoutJ.length === 1) {
+        let char = charsWithoutJ[0];
+        return (charCounts = {
+          [char]: 5,
+        });
+      } else if (charsWithoutJ.length === 4) {
+        // give to highest value card using cardRanks.findIndex to get highest value card
+        let char = charsWithoutJ.reduce((a, b) =>
+          cardRanks.findIndex((element) => element === a) >
+          cardRanks.findIndex((element) => element === b)
+            ? a
+            : b
+        );
+        charCounts[char] += jokerCount;
+        return charCounts;
+      } else {
+        // give to highest count card
+        let char = keysWithHighestValue(charCounts)[0];
+        charCounts[char] += jokerCount;
+        return charCounts;
+      }
+    case 3:
+      if (charsWithoutJ.length === 1) {
+        let char = charsWithoutJ[0];
+        return (charCounts = {
+          [char]: 5,
+        });
+      } else {
+        // give to highest value card using cardRanks.findIndex to get highest value card
+        let char = charsWithoutJ.reduce((a, b) =>
+          cardRanks.findIndex((element) => element === a) >
+          cardRanks.findIndex((element) => element === b)
+            ? a
+            : b
+        );
+        charCounts[char] += jokerCount;
+        return charCounts;
+      }
+    case 4:
+      let char = charsWithoutJ[0];
+      return (charCounts = {
+        [char]: 5,
+      });
+    case 5:
+      charCounts = {
+        A: 5,
+      };
+      return charCounts;
+    default:
+      return charCounts;
+  }
 }
 
 function findWinnings(handsCounts) {
@@ -97,9 +225,22 @@ function findWinnings(handsCounts) {
   const winnings = [];
   for (hand of handsRanked) {
     const rank = handsRanked.findIndex((element) => element === hand) + 1;
+    console.log(`Hand: ${hand} | Rank: ${rank}`);
     winnings.push(handBids.get(hand) * rank);
   }
   return winnings;
+}
+
+function keysWithHighestValue(obj) {
+  // find the highest value in the object
+  const maxValue = Math.max(...Object.values(obj));
+
+  // find all keys with the highest value
+  const keysWithMaxValue = Object.keys(obj).filter(
+    (key) => obj[key] === maxValue
+  );
+
+  return keysWithMaxValue;
 }
 
 const filePath = "input.txt";
