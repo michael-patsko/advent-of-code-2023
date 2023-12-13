b176756c07c6241e8c1999f9a34eb784dd943c33
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-13 14:41:34 +0000

    modify template file

--- template.js
+++ template.js
@@ -1,3 +1,4 @@
+let startTime = process.hrtime();
 const fs = require("fs");
 
 function readFileAndParse(filePath) {
@@ -11,3 +12,7 @@ function readFileAndParse(filePath) {
 const filePath = "test.txt";
 const lines = readFileAndParse(filePath);
 console.log(lines);
+
+let diff = process.hrtime(startTime);
+let timeInMilliseconds = diff[0] * 1000 + diff[1] / 1000000;
+console.log(`Execution time: ${timeInMilliseconds} milliseconds`);
