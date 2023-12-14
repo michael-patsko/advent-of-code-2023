327cce676dcb64335e037b436890cf620f0249ae
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-14 13:37:02 +0000

    day 13 part 2 working solution

--- Day 13/day13.js
+++ Day 13/day13.js
@@ -1,6 +1,16 @@
 let startTime = process.hrtime();
 const fs = require("fs");
 
+// Taken from https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
+// Thanks to Cem Kalyoncu: https://github.com/cemkalyoncu
+function replaceAt(string, index, replacement) {
+  return (
+    string.substring(0, index) +
+    replacement +
+    string.substring(index + replacement.length)
+  );
+}
+
 function removeFromArray(array, number) {
   return array.filter((element) => element !== number);
 }
@@ -8,16 +18,17 @@ function removeFromArray(array, number) {
 // Adapted from https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
 // Thanks to Nitin Jadhav: https://github.com/nitinja
 function rotateBlock(block, direction = "anticlockwise") {
-  block = block.split(/\r?\n/).map((element) => element.split(""));
+  let newBlock = block.split(/\r?\n/).map((element) => element.split(""));
   if (direction === "anticlockwise") {
-    block = block[0].map((val, index) =>
-      block.map((row) => row[row.length - 1 - index])
+    newBlock = newBlock[0].map((val, index) =>
+      newBlock.map((row) => row[row.length - 1 - index])
     );
-    return block.map((element) => element.join("")).join("\n");
+    return newBlock.map((element) => element.join("")).join("\n");
   } else if (direction === "clockwise") {
-    block = block[0].map((val, index) =>
-      block.map((row) => row[index]).reverse()
+    newBlock = newBlock[0].map((val, index) =>
+      newBlock.map((row) => row[index]).reverse()
     );
+    return newBlock.map((element) => element.join("")).join("\n");
   } else {
     throw new Error("Invalid rotation direction: ", direction);
   }
@@ -34,7 +45,6 @@ function readFileAndParse(filePath) {
 function findVerticalReflection(block) {
   // Define line of reflection using column number to left of line
   let lines = block.split(/\r?\n/);
-  let validReflection = true;
   let possibleColumns = [...Array(lines[0].length - 1).keys()];
   for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
     let line = lines[lineIndex];
@@ -48,35 +58,143 @@ function findVerticalReflection(block) {
       }
     }
   }
-  if (possibleColumns.length === 1) {
-    return possibleColumns[0] + 1;
+  possibleColumns = possibleColumns.map((element) => element + 1);
+  if (possibleColumns.length >= 1) {
+    return possibleColumns;
   } else if (possibleColumns.length === 0) {
-    return 0;
+    return [];
   } else {
-    throw new Error(`Too many vertical reflections in block:\n`, block);
+    throw new Error(`Error with possibleColumns: ${possibleColumns}`);
   }
 }
 
 function findHorizontalReflection(block) {
-  block = rotateBlock(block, "anticlockwise");
-  return findVerticalReflection(block);
+  let newBlock = rotateBlock(block, "anticlockwise");
+  let result = findVerticalReflection(newBlock);
+  return result;
 }
 
 const filePath = "input.txt";
 const blocks = readFileAndParse(filePath);
-let total = 0;
-for (let block of blocks) {
-  let verticalColumns = 0;
-  let horizontalRows = 0;
-  verticalColumns = findVerticalReflection(block);
-  if (verticalColumns === 0) {
-    horizontalRows = findHorizontalReflection(block);
+const totalBlocks = blocks.length;
+
+for (const part2 of [false, true]) {
+  part2 ? console.log("\n--- Part 2 ---\n") : console.log("\n--- Part 1 ---\n");
+  let total = 0;
+  let blockCounter = 0;
+  const loadingBarLength = 50; // Length of the loading bar
+  for (let j = 0; j < blocks.length; j++) {
+    let block = blocks[j];
+    let initialVertical = [];
+    let initialHorizontal = [];
+
+    initialVertical = findVerticalReflection(block);
+    initialHorizontal = findHorizontalReflection(block);
+
+    if (part2) {
+      // Part 2
+      for (let i = 0; i < block.length; i++) {
+        // Initialise copy of block, character we're looking at, and a value we will use to break out if we find the smudge
+        let newBlock = block;
+        let char = newBlock[i];
+        let found = false;
+        // Replace the character
+        if (char === "\n") {
+          continue;
+        } else if (char === ".") {
+          newBlock = replaceAt(newBlock, i, "#");
+        } else if (char === "#") {
+          newBlock = replaceAt(newBlock, i, ".");
+        }
+
+        let newVertical = findVerticalReflection(newBlock);
+        let newHorizontal = findHorizontalReflection(newBlock);
+        let newVLength = newVertical.length;
+        let newHLength = newHorizontal.length;
+
+        // We have 6 cases:
+        // Case 0: newHorizontal.length === 0; newVertical.length === 0
+        //   This is when adding the smudge has somehow ruined our old solution and not created a new one
+        // Case 1: newHorizontal.length === 0; newVertical.length === 1
+        //   This is when adding the smudge has either created a new solution or newVertical matches initialColumn
+        // Case 2: newHorizontal.length === 0; newVertical.length === 2
+        //   This is when adding the smudge has created a new solution in newVertical
+        // Case 3: newHorizontal.length === 1; newVertical.length === 0
+        //   This is when adding the smudge has either created a new solution or newHorizontal
+        //  matches initialRow
+        // Case 4: newHorizontal.length === 1; newVertical.length === 1
+        //   This is when a new solution has been created in either of them
+        // Case 5: newHorizontal.length === 2; newVertical.length === 0
+        //   This is when adding the smudge has created a new solution in newHorizontal
+
+        // Case 0
+        if (newHLength === 0 && newVLength === 0) {
+          continue;
+          // Case 1
+        } else if (newHLength === 0 && newVLength === 1) {
+          if (newVertical[0] !== initialVertical[0]) {
+            total += newVertical[0];
+            found = true;
+            blockCounter += 1;
+          } else {
+            continue;
+          }
+          // Case 2
+        } else if (newHLength === 0 && newVertical.length === 2) {
+          let index = newVertical.findIndex(
+            (element) => element !== initialVertical[0]
+          );
+          total += newVertical[index];
+          found = true;
+          blockCounter += 1;
+          // Case 3
+        } else if (newHLength === 1 && newVLength === 0) {
+          if (newHorizontal[0] !== initialHorizontal[0]) {
+            total += 100 * newHorizontal[0];
+            found = true;
+            blockCounter += 1;
+          } else {
+            continue;
+          }
+          // Case 4
+        } else if (newHLength === 1 && newVLength === 1) {
+          if (newHorizontal[0] === initialHorizontal[0]) {
+            total += newVertical[0];
+            found = true;
+            blockCounter += 1;
+          } else {
+            total += 100 * newHorizontal[0];
+            found = true;
+            blockCounter += 1;
+          }
+        }
+        // Case 5
+        else if (newHLength === 2 && newVLength === 0) {
+          let index = newHorizontal.findIndex(
+            (element) => element !== initialHorizontal[0]
+          );
+          total += 100 * newHorizontal[index];
+          found = true;
+          blockCounter += 1;
+        } else {
+          throw new Error("No new reflection found in block.");
+        }
+        if (found) break;
+      }
+    } else {
+      initialVertical.length === 0
+        ? (total += 100 * initialHorizontal[0])
+        : (total += initialVertical[0]);
+      blockCounter += 1;
+    }
+    const filledLength = Math.floor((blockCounter / 100) * loadingBarLength);
+    const bar =
+      "█".repeat(filledLength) + "-".repeat(loadingBarLength - filledLength);
+    process.stdout.write(`\r[${bar}] ${blockCounter}%`);
   }
-  total += verticalColumns;
-  total += 100 * horizontalRows;
+  console.log(`\nTotal:`, total);
 }
 
-console.log(`Total:`, total);
 let diff = process.hrtime(startTime);
 let timeInMilliseconds = diff[0] * 1000 + diff[1] / 1000000;
 console.log(`Execution time: ${timeInMilliseconds} milliseconds`);
