23e4a77601c74540539d3a97a997235332f91388
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-15 00:37:42 +0000

    day 14 part 2 working solution

--- Day 14/day14.js
+++ Day 14/day14.js
@@ -10,8 +10,16 @@ function writeMatrixToFile(matrix, filePath) {
   fs.writeFileSync(filePath, fileContent, "utf8");
 }
 
-function integerSumTo(number) {
-  return (number * (number + 1)) / 2;
+function matrixToString(matrix) {
+  return matrix.map((row) => row.join(",")).join(";");
+}
+
+function stringToMatrix(matrixString) {
+  // Split the string into rows
+  let rows = matrixString.split(";");
+
+  // Split each row into its elements
+  return rows.map((row) => row.split(","));
 }
 
 function readFileAndParse(filePath) {
@@ -21,12 +29,17 @@ function readFileAndParse(filePath) {
   return rocks;
 }
 
-function addSquareRocks(rocks) {
-  const width = rocks[0].length;
-  let squareArray = new Array(width).fill("#");
-  rocks.unshift(squareArray);
-  rocks.push(squareArray);
-  return rocks;
+function addSquareRocks(rocksGrid) {
+  const width = rocksGrid[0].length;
+  let topPadding = new Array(width).fill("#");
+  let bottomPadding = new Array(width).fill("#");
+  rocksGrid.unshift(topPadding);
+  rocksGrid.push(bottomPadding);
+  rocksGrid.forEach((row) => {
+    row.unshift("#");
+    row.push("#");
+  });
+  return rocksGrid;
 }
 
 function parseRocks(rocks) {
@@ -54,8 +67,8 @@ function findRocksBelow(rocksGrid) {
   const width = rocksGrid[0].length;
   const height = rocksGrid.length;
   let rocksBelow = new Map();
-  for (j = 0; j < width; j++) {
-    for (i = 0; i < height - 1; i++) {
+  for (let j = 0; j < width; j++) {
+    for (let i = 0; i < height - 1; i++) {
       currentRock = rocksGrid[i][j];
       if (currentRock === "O" || currentRock === ".") continue;
       // Find next square rock below and count the round rocks in between
@@ -63,7 +76,10 @@ function findRocksBelow(rocksGrid) {
       let k = i + 1;
       let belowRock = rocksGrid[k][j];
       while (belowRock !== "#") {
-        if (belowRock === "O") rocksBetween++; // Adding a round rock to the rock above
+        if (belowRock === "O") {
+          rocksBetween++; // Adding a round rock to the rock above
+          rocksGrid[k][j] = ".";
+        }
         k++;
         belowRock = rocksGrid[k][j];
       }
@@ -74,30 +90,199 @@ function findRocksBelow(rocksGrid) {
   return rocksBelow;
 }
 
-function slideNorth(rocks, squareRocks, roundRocks) {
-  let rockPairs = findRocksBelow(rocks);
+function findRocksAbove(rocksGrid) {
+  const width = rocksGrid[0].length;
+  const height = rocksGrid.length;
+  let rocksAbove = new Map();
+  for (let j = 0; j < width; j++) {
+    for (let i = height - 1; i > 0; i--) {
+      currentRock = rocksGrid[i][j];
+      if (currentRock === "O" || currentRock === ".") continue;
+      // Find next square rock below and count the round rocks in between
+      let rocksBetween = 0;
+      let k = i - 1;
+      let aboveRock = rocksGrid[k][j];
+      while (aboveRock !== "#") {
+        if (aboveRock === "O") {
+          rocksBetween++; // Adding a round rock to the rock above
+          rocksGrid[k][j] = ".";
+        }
+        k--;
+        aboveRock = rocksGrid[k][j];
+      }
+      if (rocksBetween > 0) rocksAbove.set([i, j], rocksBetween);
+      // if (k - i > 1) rockPairs.set([i, j], [k, j]); // Only map rocks with a gap between them and the rock below
+    }
+  }
+  return rocksAbove;
+}
+
+function findRocksRight(rocksGrid) {
+  const width = rocksGrid[0].length;
+  const height = rocksGrid.length;
+  let rocksRight = new Map();
+  for (let j = 0; j < width - 1; j++) {
+    for (let i = 0; i < height - 1; i++) {
+      currentRock = rocksGrid[i][j];
+      if (currentRock === "O" || currentRock === ".") continue;
+
+      let rocksBetween = 0;
+      let k = j + 1;
+      let rightRock = rocksGrid[i][k];
+      while (rightRock !== "#") {
+        if (rightRock === "O") {
+          rocksBetween++;
+          rocksGrid[i][k] = ".";
+        }
+        k++;
+        rightRock = rocksGrid[i][k];
+      }
+      if (rocksBetween > 0) rocksRight.set([i, j], rocksBetween);
+    }
+  }
+  return rocksRight;
+}
+
+function findRocksLeft(rocksGrid) {
+  const width = rocksGrid[0].length;
+  const height = rocksGrid.length;
+  let rocksLeft = new Map();
+  for (let j = width - 1; j > 0; j--) {
+    for (let i = 0; i < height - 1; i++) {
+      currentRock = rocksGrid[i][j];
+      if (currentRock === "O" || currentRock === ".") continue;
+
+      let rocksBetween = 0;
+      let k = j - 1;
+      let rightRock = rocksGrid[i][k];
+      while (rightRock !== "#") {
+        if (rightRock === "O") {
+          rocksBetween++;
+          rocksGrid[i][k] = ".";
+        }
+        k--;
+        rightRock = rocksGrid[i][k];
+      }
+      if (rocksBetween > 0) rocksLeft.set([i, j], rocksBetween);
+    }
+  }
+  return rocksLeft;
+}
+
+function slideNorth(rocksGrid) {
+  let rocksBelow = findRocksBelow(rocksGrid);
+  rocksBelow.forEach((rockBelowCount, squareRock) => {
+    let i = squareRock[0];
+    let j = squareRock[1];
+    for (let k = 1; k <= rockBelowCount; k++) {
+      rocksGrid[i + k][j] = "O";
+    }
+  });
+  return rocksGrid;
+}
+
+function slideSouth(rocksGrid) {
+  let rocksAbove = findRocksAbove(rocksGrid);
+  rocksAbove.forEach((rockAboveCount, squareRock) => {
+    let i = squareRock[0];
+    let j = squareRock[1];
+    for (let k = 1; k <= rockAboveCount; k++) {
+      rocksGrid[i - k][j] = "O";
+    }
+  });
+  return rocksGrid;
+}
+
+function slideWest(rocksGrid) {
+  let rocksRight = findRocksRight(rocksGrid);
+  rocksRight.forEach((rockRightCount, squareRock) => {
+    let i = squareRock[0];
+    let j = squareRock[1];
+    for (let k = 1; k <= rockRightCount; k++) {
+      rocksGrid[i][j + k] = "O";
+    }
+  });
+  return rocksGrid;
+}
+
+function slideEast(rocksGrid) {
+  let rocksLeft = findRocksLeft(rocksGrid);
+  rocksLeft.forEach((rockLeftCount, squareRock) => {
+    let i = squareRock[0];
+    let j = squareRock[1];
+    for (let k = 1; k <= rockLeftCount; k++) {
+      rocksGrid[i][j - k] = "O";
+    }
+  });
+  return rocksGrid;
+}
+
+function spinCycle(rocksGrid, number) {
+  let previousStates = new Map();
+  let cycles = 0;
+  let loopStart = -1;
+  let loopEnd = -1;
+  for (let i = 0; i < number; i++) {
+    rocksGrid = slideNorth(rocksGrid);
+    rocksGrid = slideWest(rocksGrid);
+    rocksGrid = slideSouth(rocksGrid);
+    rocksGrid = slideEast(rocksGrid);
+    cycles++;
+    let currentState = matrixToString(rocksGrid);
+    if (previousStates.has(currentState)) {
+      loopStart = previousStates.get(currentState);
+      loopEnd = cycles;
+      break;
+    }
+    previousStates.set(currentState, cycles);
+  }
+  let finalGrid = rocksGrid.map((arr) => arr.slice());
+  return { finalGrid, loopStart, loopEnd, previousStates };
+}
+
+function calculateNorthLoad(rocksGrid) {
+  let totalLoad = 0;
+  const height = rocksGrid.length;
+  const width = rocksGrid[0].length;
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (rocksGrid[i][j] === "O") {
+        totalLoad += height - i - 1;
+      }
+    }
+  }
+  return totalLoad;
 }
 
 function main(fileName) {
   const filePath = `${fileName}.txt`;
   let rocksGrid = readFileAndParse(filePath);
-  // Pad top and bottom of rocks array with square rocks
-  // I have a feeling I'll regret this in part 2
-  // If I want to pad to sides, I should use .map(element => element.unshift("#").push("#"))
   rocksGrid = addSquareRocks(rocksGrid);
   const { squareRocks, roundRocks } = parseRocks(rocksGrid);
-  const rocksBelow = findRocksBelow(rocksGrid);
 
-  // Now we calculate total load on North supports
-  let totalLoad = 0;
-  const height = rocksGrid.length;
-  rocksBelow.forEach((rockBelowCount, squareRock) => {
-    let i = squareRock[0];
-    totalLoad +=
-      rockBelowCount * (height - i - 1) - integerSumTo(rockBelowCount);
+  let numCycles = 1000000000;
+  const { finalGrid, loopStart, loopEnd, previousStates } = spinCycle(
+    rocksGrid,
+    numCycles
+  );
+  const loopLength = loopEnd - loopStart;
+  const remainingCycles = (numCycles - loopStart) % loopLength;
+  const finalStateCycle = loopStart + remainingCycles;
+
+  let finalStateString = "";
+  previousStates.forEach((value, key) => {
+    if (value === finalStateCycle) finalStateString = key;
   });
-  // console.log(rocksBelow);
-  console.log(totalLoad);
+
+  const finalState = stringToMatrix(finalStateString);
+  const totalLoad = calculateNorthLoad(finalState);
+
+  console.log(
+    `Total load on north supports after ${numCycles} cycles:`,
+    totalLoad
+  );
+
+  writeMatrixToFile(finalGrid, "output.txt");
 }
 
 main("input");
