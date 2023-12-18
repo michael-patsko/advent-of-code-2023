c134d2ac41ee53645df17cc281912ed048b5fe62
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-12-18 21:16:13 +0000

    day 18 part 2 working solution

--- Day 18/day18.js
+++ Day 18/day18.js
@@ -21,27 +21,18 @@ function stringToCoordinate(str) {
 function readFileAndParse(filePath) {
   const content = fs.readFileSync(filePath, "utf8");
   const lines = content.split(/\r?\n/);
+  const directions = ["R", "D", "L", "U"];
   let instructions = [];
   lines.forEach((line) => {
-    let direction = line.match(/\w(?=\s\d)/g)[0];
-    let distance = Number(line.match(/(?<=\w\s)\d+/g)[0]);
-    let colourCode = line.match(/(?<=\()#[\w\d]+(?=\))/g)[0];
+    let directionIndex = line.match(/(?<=#[\w\d]{5})\w{1}/g)[0];
+    let direction = directions[directionIndex];
+    let distance = parseInt(line.match(/(?<=#)[\w\d]{5}/g)[0], 16);
     instructions.push({
       direction: direction,
       distance: distance,
-      colourCode: colourCode,
     });
   });
   return instructions;
-}
-
-function generateMatrix(width, height, character) {
-  let matrix = [];
-  for (let i = 0; i < height; i++) {
-    let row = new Array(width).fill(character);
-    matrix.push(row);
-  }
-  return matrix;
 }
 
 function findBoundsAndStartPoint(instructions) {
@@ -88,83 +79,70 @@ function followDirections(instructions) {
   const { width, height, startX, startY } =
     findBoundsAndStartPoint(instructions);
 
-  // Generate a matrix that represents our lagoon
-  let lagoon = generateMatrix(width, height, ".");
+  let startCoord = [startY, startX];
+  let currentCoord = startCoord;
+  let totalArea = 0;
 
-  let currentCoord = [startY, startX];
-  let lagoonEdges = new Map();
   for (let n = 0; n < instructions.length; n++) {
     const instruction = instructions[n];
     const direction = instruction.direction;
     const distance = instruction.distance;
-    const colourCode = instruction.colourCode;
     const i = currentCoord[0];
     const j = currentCoord[1];
+    let previousCoord = [i, j];
 
     switch (direction) {
       case "R":
         for (let k = 1; k <= distance; k++) {
-          lagoonEdges.set(coordinateToString([i, j + k]), colourCode);
-          lagoon[i][j + k] = "#";
+          previousCoord = [currentCoord[0], currentCoord[1]];
+          currentCoord[1] = j + k;
+          totalArea += determinantOfPoints(previousCoord, currentCoord);
         }
-        currentCoord[1] = j + distance;
         break;
       case "L":
         for (let k = 1; k <= distance; k++) {
-          lagoonEdges.set(coordinateToString([i, j - k]), colourCode);
-          lagoon[i][j - k] = "#";
+          previousCoord = [currentCoord[0], currentCoord[1]];
+          currentCoord[1] = j - k;
+          totalArea += determinantOfPoints(previousCoord, currentCoord);
         }
-        currentCoord[1] = j - distance;
         break;
       case "U":
         for (let k = 1; k <= distance; k++) {
-          lagoonEdges.set(coordinateToString([i - k, j]), colourCode);
-          lagoon[i - k][j] = "#";
+          previousCoord = [currentCoord[0], currentCoord[1]];
+          currentCoord[0] = i - k;
+          totalArea += determinantOfPoints(previousCoord, currentCoord);
         }
-        currentCoord[0] = i - distance;
         break;
       case "D":
         for (let k = 1; k <= distance; k++) {
-          lagoonEdges.set(coordinateToString([i + k, j]), colourCode);
-          lagoon[i + k][j] = "#";
+          previousCoord = [currentCoord[0], currentCoord[1]];
+          currentCoord[0] = i + k;
+          totalArea += determinantOfPoints(previousCoord, currentCoord);
         }
-        currentCoord[0] = i + distance;
         break;
       default:
         throw new Error(`Invalid direction: ${instruction.direction}`);
     }
   }
-  return { lagoon, lagoonEdges };
+  totalArea += determinantOfPoints(currentCoord, startCoord);
+  return Math.abs(totalArea / 2);
 }
 
-function determinantOfPoints(coord1, coord2, matrix) {
-  // coord [i, j], where i is column and j is row
-  // suppose we have [1 ,2] for 7 by 7 matrix
-  // this is equivalent to (2, 6)
-  // for [i, j] we have (j, height - i)
-
-  let height = matrix.length;
-
-  let x1 = coord1[1];
-  let x2 = coord2[1];
-  let y1 = height - coord1[0];
-  let y2 = height - coord2[0];
-
-  let a = x1;
-  let b = x2;
-  let c = y1;
-  let d = y2;
+function determinantOfPoints(coord1, coord2) {
+  let a = coord1[1];
+  let b = coord2[1];
+  let c = coord1[0];
+  let d = coord2[0];
   return a * d - b * c;
 }
 
-function findArea(edgePoints, matrix) {
+function findArea(edgePoints) {
   let area = determinantOfPoints(
     edgePoints[edgePoints.length - 1],
-    edgePoints[0],
-    matrix
+    edgePoints[0]
   );
   for (i = 0; i < edgePoints.length - 1; i++) {
-    area += determinantOfPoints(edgePoints[i], edgePoints[i + 1], matrix);
+    area += determinantOfPoints(edgePoints[i], edgePoints[i + 1]);
   }
   return Math.abs(area / 2);
 }
@@ -174,26 +152,22 @@ function main(fileName) {
   const instructions = readFileAndParse(filePath);
 
   // Fill in lagoon edges with their colour data
-  const { lagoon, lagoonEdges } = followDirections(instructions);
-  writeMatrixToFile(lagoon, "output.txt");
+  const totalArea = followDirections(instructions);
 
-  // Convert lagoonEdges coordinates to form usable by findArea
-  const edgeCoordinates = Array.from(lagoonEdges.keys()).map((key) =>
-    stringToCoordinate(key)
-  );
-
-  // We will find the interior points using Pick's theorem with the shoelace formula; similar to day 10
-  const lagoonArea = findArea(edgeCoordinates, lagoon);
+  let exteriorPoints = 0;
+  instructions.forEach((instruction) => {
+    exteriorPoints += instruction.distance;
+  });
 
   // Number of interior points is given by i = A + 1 - b/2, where A is the area and b is the number of points on the boundary
-  const interiorPoints = lagoonArea + 1 - edgeCoordinates.length / 2;
+  const interiorPoints = totalArea + 1 - exteriorPoints / 2;
 
   // Add the number of points on edge to interior points to give us total lagoon volume
-  const lagoonVolume = edgeCoordinates.length + interiorPoints;
+  const lagoonVolume = exteriorPoints + interiorPoints;
   console.log(`Lagoon volume:`, lagoonVolume);
 }
 
-main("test");
+main("input");
 
 let diff = process.hrtime(startTime);
 let timeInMilliseconds = diff[0] * 1000 + diff[1] / 1000000;
